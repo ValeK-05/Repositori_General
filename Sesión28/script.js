505fb00158ef108e6a55565d469d9ef3b067d3e6
Daniela <danpinedacampos@crackthecode.la>
2024-08-12 12:02:19 -0500

    Se agregó sesión28

---
+++ Sesión28/script.js
@@ -0,0 +1,9 @@
+
+function sumar() {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+
+    
+
+    document.getElementById("resultado").innerHTML = parseInt(num1)+ parseInt(num2);
+}
